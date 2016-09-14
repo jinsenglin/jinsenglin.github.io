f01289e8dcfa624e9d6f0770117cd056ab81d922
Jim Lin <jimlintw922@gmail.com>
2016-09-14 14:16:26 +0800

    applied color

--- gd/os/pkg_deps/reqtxttodotstr.py
+++ gd/os/pkg_deps/reqtxttodotstr.py
@@ -8,23 +8,28 @@ GITHUB = 'https://raw.githubusercontent.com'
 PROJECT = 'openstack/nova'
 BRANCH = 'stable/mitaka'
 REQTXT = 'requirements.txt'
+COLOR = '#97C2FC'
 
 pkgs = [
-    ('keystone', 'openstack/keystone', 'stable/mitaka'),
-    ('glance', 'openstack/nova', 'stable/mitaka'),
-    ('nova', 'openstack/nova', 'stable/mitaka'),
-    ('cinder', 'openstack/nova', 'stable/mitaka'),
-    ('neutron', 'openstack/nova', 'stable/mitaka'),
-    ('horizon', 'openstack/nova', 'stable/mitaka'),
-    ('swift', 'openstack/nova', 'stable/mitaka'),
-    ('ceilometer', 'openstack/nova', 'stable/mitaka'),
+    ('keystone', 'openstack/keystone', 'stable/mitaka', '#97C2FC'),
+    ('glance', 'openstack/nova', 'stable/mitaka', '#FFFF00'),
+    ('nova', 'openstack/nova', 'stable/mitaka', '#FB7E81'),
+    ('cinder', 'openstack/nova', 'stable/mitaka', '#7BE141'),
+    ('neutron', 'openstack/nova', 'stable/mitaka', '#6E6EFD'),
+    ('horizon', 'openstack/nova', 'stable/mitaka', '#C2FABC'),
+    ('swift', 'openstack/nova', 'stable/mitaka', '#FFA807'),
+    ('ceilometer', 'openstack/nova', 'stable/mitaka', '#6E6EFD'),
 ]
 
-with open('docstr.js', 'w') as js_file:
-    js_file.write("var DOTstring = 'dinetwork { \\\n")
+with open('nodes.js', 'w') as nodes_file, open('edges.js', 'w') as edges_file:
+# with open('docstr.js', 'w') as js_file:
+    nodes_file.write("var nodes = new vis.DataSet([\n")
+    edges_file.write("var edges = new vis.DataSet([\n")
+    # js_file.write("var DOTstring = 'dinetwork { \\\n")
 
+    nodes = set()
     for pkg in pkgs:
-        NAME, PROJECT, BRANCH = pkg
+        NAME, PROJECT, BRANCH, COLOR = pkg
         url = '/'.join([GITHUB, PROJECT, BRANCH, REQTXT])
 
         txt = '-'.join([NAME, REQTXT])
@@ -32,8 +37,18 @@ with open('docstr.js', 'w') as js_file:
 
         reqs = parse_requirements(txt, session=False)
         deps = [str(req.req.name) for req in reqs]
-        for dep in deps:
-            js_file.write(NAME + '->' + dep.replace('-', '_') + ';\\\n')
-            print NAME + '->' + dep.replace('-', '_') + ';\\'
 
-    js_file.write("}';")
+        nodes_file.write("{id: '" + NAME + "', label: '" + NAME + "', color:'" + COLOR + "'},\n")
+        for dep in deps:
+            renamed_dep = dep.replace('-', '_')
+            nodes.add(renamed_dep)
+            edges_file.write("{from: '" + NAME + "', to: '" + renamed_dep + "', color:{highlight:'" + COLOR + "'}},\n")
+            # js_file.write(NAME + '->' + dep.replace('-', '_') + ';\\\n')
+            print NAME + '->' + renamed_dep + ';\\'
+
+    for dep in nodes:
+        nodes_file.write("{id: '" + dep + "', label: '" + dep + "'},\n")
+
+    nodes_file.write("]);")
+    edges_file.write("]);")
+    # js_file.write("}';")
